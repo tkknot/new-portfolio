b651552bb8bdf96b4040066bb5d8f97c9d479cdf
tkknot <takeda19991015@gmail.com>
2021-01-03 10:37:42 +0900

    save

--- components/layouts/header.vue
+++ components/layouts/header.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-      <v-navigation-drawer app>
-
-  </v-navigation-drawer>
-
-  <v-app-bar app>
-
+  <v-app-bar app height="500" src="/img/home.jpg" absolute>
+  <v-col cols='12'>
+  <v-app-bar-title>aaa</v-app-bar-title>
+  </v-col>
+  <v-tabs align-with-title>
+    <v-tab>a</v-tab>
+    <v-tab>b</v-tab>
+    <v-tab>c</v-tab>
+    <v-tab>d</v-tab>
+  </v-tabs>
   </v-app-bar>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    
+    <h1>Profile</h1>
+    <v-container cols='12'></v-container>
+    <h1>Programming Skill</h1>
+    <v-container cols='12'></v-container>
+    <h1>Personal Skill</h1>
+    <v-container cols='12'></v-container>
+    <h1>Portfolio</h1>
+    <v-container cols='12'></v-container>
   </div>
 </template>
