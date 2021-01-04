5ba3a49020e1d517c2caf04f60d328e3000a47b4
tkknot <takeda19991015@gmail.com>
2021-01-04 17:49:24 +0900

    save

--- components/portfolio.vue
+++ components/portfolio.vue
@@ -8,19 +8,33 @@
           :src="photo.src"
           :key="i"
           nuxt
-          :to="photo.link"
           transition="fade-transition"
         >
-          <v-sheet tile :color="photo.color">
-            <div class="display-2 text-h6 pl-3">
-              {{ photo.title }}
-            </div>
-          </v-sheet>
+          <a :href="photo.link" id="link" class="white--text">
+            <v-sheet tile :color="photo.color">
+              <div class="display-2 text-h6 pl-3" id="link-box">
+                <span>{{ photo.title }}</span>
+              </div>
+            </v-sheet>
+          </a>
         </v-carousel-item>
       </v-carousel>
     </v-container>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#link {
+  text-decoration: none;
+}
+
+#link-box {
+  &:hover {
+    cursor: pointer;
+    background: rgba(134, 227, 255, 0.705);
+  }
+}
+</style>
 
 <script>
 export default {
@@ -29,13 +43,13 @@ export default {
       photos: [
         {
           src: "/img/logo-new.jpeg",
-          link: "",
+          link: "https://kassa-yuu.com/",
           title: "kassa-yuu",
           color: "pink lighten-2"
         },
         {
           src: "/img/workout.jpg",
-          link: "",
+          link: "https://self-train.com/",
           title: "Carories App",
           color: "yellow lighten-2"
         }
