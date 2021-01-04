f841a6dfd6007eb2bd9e2b652cc1f1197f27af24
tkknot <takeda19991015@gmail.com>
2021-01-04 17:09:05 +0900

    save

--- components/personal.vue
+++ components/personal.vue
@@ -3,8 +3,37 @@
     <h1 class="text-center">Personal Skill</h1>
         <v-container class="mt-12">
       <v-row>
-
+        <v-col cols="3" class="mx-auto" v-for="(item, i) in items" :key=i>
+          <v-card height="480">
+            <v-img :src="item.src"></v-img>
+            <h2 class="text-center font-weight-regular py-1">{{item.h2}}</h2>
+            <v-card-text class="text-h6">{{item.text}}</v-card-text> 
+          </v-card>
+        </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      items: [
+        {src: "/img/study.svg",
+          h2: 'Qualification',
+          text: '金融系から語学系まで、6つの資格を持っています。'
+        },
+        {src: "/img/macho.svg",
+          h2: 'Stoic',
+          text: '食事管理とライフハックを日常的に行っているので、常にベストコンディションです。'
+        },
+        {src: "/img/canhoo.svg",
+          h2: 'Greedy',
+          text: '好奇心旺盛で、やりたいと思ったことには果敢に取り組みます。'
+        },
+      ]
+    }
+  }
+}
+</script>
--- components/profile.vue
+++ components/profile.vue
@@ -4,15 +4,29 @@
     <v-container class="mt-12">
       <v-row>
         <v-col cols="6">
-            <v-img src="/img/me.jpeg"  id="photo-box" class="rounded-xl mx-auto"></v-img>
+          <v-img
+            src="/img/me.jpeg"
+            id="photo-box"
+            class="rounded-xl mx-auto"
+          ></v-img>
         </v-col>
         <v-col cols="6">
-          <p class="text-h6">現在21歳、読書が好きだということをきっかけに、<br>
-          世田谷の大学で文学を専攻している文系大学生です。</p>
-          <p class="text-h6">筋トレとライフハックが好きで、<br>自己管理能力には定評があります。</p>
-          <p class="text-h6">学生時代は2つの団体運営と6つの資格取得など、<br>複数活動に注力していました。</p>
-            <p class="text-h6">現在、フロントエンドの技術をメインに、<br>プログラミングを独学で勉強しています。</p>
-          <p class="text-h6">また、自分の知識や熱量で人の役に立ちたいという思いから、<br>エンジニア職を見ています。</p>
+          <p class="text-h6">
+            現在21歳、読書が好きだということをきっかけに、<br />
+            世田谷の大学で文学を専攻している文系大学生です。
+          </p>
+          <p class="text-h6">
+            筋トレとライフハックが好きで、<br />自己管理能力には定評があります。
+          </p>
+          <p class="text-h6">
+            学生時代は2つの団体運営と6つの資格取得など、<br />複数活動に注力していました。
+          </p>
+          <p class="text-h6">
+            現在、フロントエンドの技術をメインに、<br />プログラミングを独学で勉強しています。
+          </p>
+          <p class="text-h6">
+            また、自分の知識や熱量で人の役に立ちたいという思いから、エンジニア職を見ています。
+          </p>
         </v-col>
       </v-row>
     </v-container>
@@ -20,8 +34,8 @@
 </template>
 
 <style lang="scss">
-  #photo-box {
-    width: 80%;
-    height: 90%;
-  }
+#photo-box {
+  width: 80%;
+  height: 84%;
+}
 </style>
